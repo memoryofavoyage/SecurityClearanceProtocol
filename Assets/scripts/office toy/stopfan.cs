e410e008a7a2a7308fab7904df8f54df47c60c57
memoryofavoyage <ojtaylor2004@gmail.com>
2026-03-27 11:09:11 +0000

    fix fan

--- Assets/scripts/office toy/stopfan.cs
+++ Assets/scripts/office toy/stopfan.cs
@@ -9,8 +9,8 @@ public class stopfan : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        fansound.Play();
         fansound = GetComponent<AudioSource>();
+        fansound.Play();
         fansound.clip = clip;
     }
 
